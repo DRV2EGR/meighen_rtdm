f3e2a163962cfae74b4ff970209102fb95b305cd
Dmitry <1664286@gmail.com>
2022-12-01 03:54:44 +0300

    MGN-1 | Основной презентер добавлен

---
+++ presenter/src/test/java/io/meighen/presenter/PresenterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meighen.presenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
