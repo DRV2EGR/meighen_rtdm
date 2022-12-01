05b4cd31e8f8b2b06a766918b9d04b5412de4cca
Dmitry <1664286@gmail.com>
2022-12-01 03:55:53 +0300

    MGN-1 | Добавлен сервис сборки и развертывания

---
+++ service_bulder/src/main/java/io/meighen/service_bulder/ServiceBulderApplication.java
@@ -0,0 +1,13 @@
+package io.meighen.service_bulder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBulderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBulderApplication.class, args);
+	}
+
+}
---
+++ service_bulder/src/test/java/io/meighen/service_bulder/ServiceBulderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.meighen.service_bulder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBulderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
