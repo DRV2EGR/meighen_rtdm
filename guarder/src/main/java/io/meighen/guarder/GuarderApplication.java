7d567b33af384aee6956c94ddbaa497665917222
Dmitry <1664286@gmail.com>
2022-12-01 03:55:15 +0300

    MGN-1 | Сервис авторизации добавлен

---
+++ guarder/src/main/java/io/meighen/guarder/GuarderApplication.java
@@ -0,0 +1,13 @@
+package io.meighen.guarder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuarderApplication.class, args);
+	}
+
+}
